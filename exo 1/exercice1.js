97bba6acd2e539fd6cfe162930d338df832b2d76
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2020-11-30 15:47:32 +0100

    Exo-58

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,9 +1,15 @@
 
-(fruits) {
-    "Banana":
-    alert("Hello")
-    break;
-    "Apple":
-    alert("Welcome")
-    break;
+
+let fruits = prompt("Entrez 'banana' ou 'apple'");
+
+
+switch(fruits) {
+    case 'banana':
+        alert("Hello");
+        break;
+    case 'apple':
+        alert("Welcomme");
+        break;
+    default:
+        alert("On se fout de moi");
 }
--- exo 2/exercice2.js
+++ exo 2/exercice2.js
@@ -1,9 +1,14 @@
 
-(fruits) {
-    "Banana":
-    alert("Hello")
-    break;
-    "Apple":
-    alert("Welcome")
-    break;
+let fruits = prompt("Entrez 'banana' ou 'apple'");
+
+
+switch(fruits) {
+    case 'banana':
+        alert("Hello");
+        break;
+    case 'apple':
+        alert("Welcomme");
+        break;
+    default:
+        alert("aucun des deux");
 }
